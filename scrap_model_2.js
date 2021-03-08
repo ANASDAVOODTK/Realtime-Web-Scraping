e1e0e2a7a6fb71b297b182907b393f71b4fd1cb9
ANASDAVOODTK <42793750+ANASDAVOODTK@users.noreply.github.com>
2021-03-08 11:17:19 +0530

    Update scrap_model_2.js

--- scrap_model_2.js
+++ scrap_model_2.js
@@ -1,1 +1,30 @@
 
+const lib = require('lib')({token: process.env.STDLIB_SECRET_TOKEN});
+
+const result = {crawler: {}};
+
+console.log(`Running [Crawler → Query (scrape) a provided URL based on CSS selectors]...`);
+result.crawler.pageData = await lib.crawler.query['@0.0.1'].selectors({
+  url: `https://news.ycombinator.com/`,
+  userAgent: `stdlib/crawler/query`,
+  includeMetadata: false,
+  selectorQueries: [
+    {
+      'selector': `tr[id]:not([id="pagespace"])`,
+      'resolver': `map`,
+      'mapQueries': [
+        {
+          'selector': 'a.storylink',
+          'resolver': 'text'
+        },
+        {
+          'selector': 'a.storylink',
+          'resolver': 'attr',
+          'attr': 'href'
+        }
+      ]
+    }
+  ]
+});
+
+return result;
